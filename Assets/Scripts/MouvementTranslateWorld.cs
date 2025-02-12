588e3f7dd87912515aa18ff847fa4a70d0502126
Victor4387 <victordinezza@videotron.ca>
2025-02-12 14:42:56 -0500

    exercice 1

--- Assets/Scripts/MouvementTranslateWorld.cs
+++ Assets/Scripts/MouvementTranslateWorld.cs
@@ -4,11 +4,14 @@ using UnityEngine;
 
 public class MouvementTranslateWorld : MonoBehaviour
 {
+    [SerializeField] Vector3 direction;
+    [SerializeField] float vitesse;
     void Update()
     {
-        transform.Translate(1f * Time.deltaTime, 0, 0, Space.World);
+        //transform.Translate(1f * Time.deltaTime, 0, 0,Space.World);
 
         //Vector3 deplacement = new Vector3(1f, 0, 0);
         //transform.Translate(deplacement * Time.deltaTime, Space.World);
+        transform.Translate(direction.normalized * vitesse * Time.deltaTime, Space.Self);
     }
 }
